28dbdbc5c72a85af14016f69b6b4ce4d6a241249
soumyachhetri2513-debug <soumyachhetri2513@gmail.com>
2026-08-20 21:56:11 +0530

    Add pnz program

---
+++ pnz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n >= 0) {
+        if (n == 0) {
+            printf("%d is zero\n", n);
+        } else {
+            printf("%d is positive\n", n);
+        }
+    } else {
+        printf("%d is negative\n", n);
+    }
+
+    return 0;
+}
