2a7a927007691d03681b74292a9ada578cae1c00
soumyachhetri05 <soumyachhetri05@gmail.com>
2026-09-07 12:48:47 +0530

    Add day15 program

--- day15.c
+++ day15.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main() {
-    int n, product = 1;
+    int n;
+    long long factorial = 1;
 
     printf("Enter a number: \n");
     scanf("%d", &n);
 
-    for(int i = 2; i <= n; i++) {
-        product = product * i;
+    for(int i = 1; i <= n; i++) {
+        factorial = factorial * i;
     }
-    printf("Product: %d\n", product);
-    return 0;
+   printf("Factorial: %lld\n", factorial);
+   return 0;
 }
