9695482501f51872eb75df475058b05744c8e1d2
soumyachhetri05 <soumyachhetri05@gmail.com>
2026-09-07 13:00:51 +0530

    Add d15 program

---
+++ d15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, reverse = 0, digit;
+
+   printf("Enter a number: \n");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        digit = n % 10; 
+        reverse = reverse * 10 + digit;
+        n /= 10;
+        
+    }
+    printf("Reverse: %d\n", reverse);
+    return 0;
+}
