83e39a3975cc9315e1244e910a5385fb8262be11
soumyachhetri05 <soumyachhetri05@gmail.com>
2026-09-07 22:34:49 +0530

    Add d17 program

---
+++ d17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count++;
+        }
+    }
+
+    if (count == 2) {
+        printf("Prime number\n");
+    }
+    else {
+        printf("Not a prime number\n");
+    }
+
+    return 0;
+}
